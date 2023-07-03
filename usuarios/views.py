78fd0418a6efd09bc02639320ad0216e30bd31e2
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-03 17:27:32 -0300

    aa

--- usuarios/views.py
+++ usuarios/views.py
@@ -60,3 +60,8 @@ def cadastro(request):
             return redirect('login')
           
     return render(request, "usuarios/cadastro.html", {"form": form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso")
+    return redirect('login')
