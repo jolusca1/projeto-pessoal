e5bc081eaccba00abc36a12b80791128a8122f0e
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-04 00:10:26 -0300

    validacao

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -74,3 +74,13 @@ class CadastroForms(forms.Form):
             }
         )
     )
+    
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+        
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Não é possível inserir espaços dentro do campo usuário")
+            else:
+                return nome
