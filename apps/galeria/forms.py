ff50a2248a28eec1d547016372817d56c6a829ac
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-06 17:42:19 -0300

    forms

---
+++ apps/galeria/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from apps.galeria.models import Fotografia
+
+
+class FotografiaForms(forms.ModelForm):
+    
+    
+    class Meta:
+        model = Fotografia
+        exclude = ['publicada',]
+        
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control'
+                }),
+            'legenda': forms.TextInput(attrs={
+                'class': 'form-control'
+                }),
+            'categoria': forms.Select(attrs={
+                'class': 'form-control'
+                }),
+            'descricao': forms.Textarea(attrs={
+                'class': 'form-control'
+                }),
+            'foto': forms.FileInput(attrs={
+                'class': 'form-control'
+                }),
+            'data_fotografia': forms.DateInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'date'
+                },
+                format='%d/%m/%Y'
+            ),
+            'usuario': forms.Select(attrs={
+                'class': 'form-control'
+                })
+        }
